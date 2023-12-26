e15fb8f7aa5beef46b2e47cfedadd122e8c8da18
sachinkumar53 <kr771.sachin@gmail.com>
2023-12-26 09:39:53 +0530

    terms and conditions added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,6 +9,10 @@ interface LoginProps {
     onPhoneNumberSubmit: (phoneNumber: string) => void;
 }
 
+const PrimaryLink = styled("a")(({ theme }) => ({
+    color: theme.palette.primary.main,
+    textDecoration: "none"
+}));
 
 function Login({ isLoading, onPhoneNumberSubmit }: LoginProps) {
     const [phoneNumber, setPhoneNumber] = useState("");
@@ -62,7 +66,10 @@ function Login({ isLoading, onPhoneNumberSubmit }: LoginProps) {
                 onChange={handlePhoneNumberChange}
                 inputProps={{ inputMode: 'numeric', pattern: '[0-9]{0,10}' }}
             />
-            <div className="recaptcha-container" />
+            <Typography variant="body2"
+                sx={{ mt: 2, mx: 1 }}>
+                By continuing, you agree to Meera's <PrimaryLink href="https://sites.google.com/view/meera-terms-and-conditions" target="_blank">Terms & Conditions </PrimaryLink> and <PrimaryLink href="https://sites.google.com/view/meera-privacy-policy" target="_blank">Privacy Policy</PrimaryLink>.
+            </Typography>
             <Box sx={{ m: 1, position: 'relative', width: "100%" }}>
                 <Button
                     id="sign-in-button"
@@ -70,10 +77,9 @@ function Login({ isLoading, onPhoneNumberSubmit }: LoginProps) {
                     fullWidth
                     disabled={isLoading || !validated}
                     variant="contained"
-                    sx={{ mt: 2, mb: 2 }}>
+                    sx={{ mt: 2 }}>
                     Send OTP
                 </Button>
-
                 {isLoading && (
                     <CircularProgress
                         color="secondary"
@@ -82,7 +88,7 @@ function Login({ isLoading, onPhoneNumberSubmit }: LoginProps) {
                             position: 'absolute',
                             top: '50%',
                             left: '50%',
-                            marginTop: '-12px',
+                            marginTop: '-4px',
                             marginLeft: '-12px',
                         }}
                     />
